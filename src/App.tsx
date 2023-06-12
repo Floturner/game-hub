0673b2857eb2b16c20a243ec7f01b2f1b88b11a6
Florent Taboukouna <56768827+Floturner@users.noreply.github.com>
2023-06-12 12:42:40 +0000

    Installed Chakra UI

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@chakra-ui/react';
+
+const App = () => {
+  return (
+    <div>
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
+};
+
+export default App;
