0eee91d663ae448bfc535dd860d9bca891b8bf7c
Florent Taboukouna <56768827+Floturner@users.noreply.github.com>
2023-06-16 17:51:41 +0000

    Customize the theme to get darker grays

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -6,6 +6,7 @@ export interface Genre {
   image_background: string;
 }
 
+// const useGenres = () => useData<Genre>('/genres');
 const useGenres = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGenres;
