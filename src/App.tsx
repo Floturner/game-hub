ac6397a25423e2d73404631675fb7a44fdb69989
Florent Taboukouna <56768827+Floturner@users.noreply.github.com>
2023-06-16 18:05:15 +0000

    Update app title

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import { Box, Flex, Grid, GridItem, Show } from '@chakra-ui/react';
+import { Genre } from './hooks/useGenres';
+import { Platform } from './hooks/usePlatforms';
 import NavBar from './components/NavBar';
 import GameGrid from './components/GameGrid';
 import GenreList from './components/GenreList';
 import PlatformSelector from './components/PlatformSelector';
-import { Genre } from './hooks/useGenres';
-import { Platform } from './hooks/usePlatforms';
 import SortSelector from './components/SortSelector';
 import GameHeading from './components/GameHeading';
 
