08ed22081dc478d94101d2c8402ea0bc76a6df2f
Florent Taboukouna <56768827+Floturner@users.noreply.github.com>
2023-06-22 17:53:03 +0000

    Fetch game details

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -7,6 +7,8 @@ import useGameQueryStore from '../store';
 export interface Game {
   id: number;
   name: string;
+  slug: string;
+  description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,21 @@
+import { Heading, Spinner, Text } from '@chakra-ui/react';
+import { useParams } from 'react-router-dom';
+import useGame from '../hooks/useGame';
+
 const GameDetailPage = () => {
-  return <div>GameDetailPage</div>;
+  const { slug } = useParams();
+  const { data: game, error, isLoading } = useGame(slug!);
+
+  if (isLoading || !game) return <Spinner />;
+
+  if (error) throw error;
+
+  return (
+    <>
+      <Heading>{game.name}</Heading>
+      <Text>{game.description_raw}</Text>
+    </>
+  );
 };
 
 export default GameDetailPage;
