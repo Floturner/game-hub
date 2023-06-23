6f405b91d834c79f8f7ea82bf36e7f5eb6c74857
Florent Taboukouna <56768827+Floturner@users.noreply.github.com>
2023-06-23 09:44:26 +0000

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import Genre from './Genre';
 import Platform from './Platform';
+import Publisher from './Publisher';
 
 export default interface Game {
   id: number;
@@ -6,7 +8,9 @@ export default interface Game {
   slug: string;
   description_raw: string;
   background_image: string;
+  genres: Genre[];
   parent_platforms: { platform: Platform }[];
+  publishers: Publisher[];
   metacritic: number;
   rating_top: number;
 }
